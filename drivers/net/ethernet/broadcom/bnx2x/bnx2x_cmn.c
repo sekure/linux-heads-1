54c943f958dba97841c48d92bd797ed1ede6fd3b
parazyd <parazyd@dyne.org>
2017-06-21 12:24:55 +0200

    deblob

--- drivers/net/ethernet/broadcom/bnx2x/bnx2x_cmn.c
+++ drivers/net/ethernet/broadcom/bnx2x/bnx2x_cmn.c
@@ -2366,27 +2366,6 @@ int bnx2x_compare_fw_ver(struct bnx2x *bp, u32 load_code, bool print_err)
 
 		/*(DEBLOBBED)*/
 
-/* returns the "mcp load_code" according to global load_count array */
-static int bnx2x_nic_load_no_mcp(struct bnx2x *bp, int port)
-{
-	int path = BP_PATH(bp);
-
-	DP(NETIF_MSG_IFUP, "NO MCP - load counts[%d]      %d, %d, %d\n",
-	   path, bnx2x_load_count[path][0], bnx2x_load_count[path][1],
-	   bnx2x_load_count[path][2]);
-	bnx2x_load_count[path][0]++;
-	bnx2x_load_count[path][1 + port]++;
-	DP(NETIF_MSG_IFUP, "NO MCP - new load counts[%d]  %d, %d, %d\n",
-	   path, bnx2x_load_count[path][0], bnx2x_load_count[path][1],
-	   bnx2x_load_count[path][2]);
-	if (bnx2x_load_count[path][0] == 1)
-		return FW_MSG_CODE_DRV_LOAD_COMMON;
-	else if (bnx2x_load_count[path][1 + port] == 1)
-		return FW_MSG_CODE_DRV_LOAD_PORT;
-	else
-		return FW_MSG_CODE_DRV_LOAD_FUNCTION;
-}
-
 /* mark PMF if applicable */
 static void bnx2x_nic_load_pmf(struct bnx2x *bp, u32 load_code)
 {
